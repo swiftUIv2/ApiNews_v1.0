b1e8fa2908f39120974299690ded0d77d23e741d
emm <723125@gmail.com>
2021-02-15 14:49:16 +0000

    Initial Commit

---
+++ ApiNews_v1.0/ApiNews_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ApiNews_v1_0App.swift
+//  ApiNews_v1.0
+//
+//  Created by emm on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ApiNews_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
